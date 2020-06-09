8650470692bdbe47fbce33a940df3e5157ec8601
miho.takahashi3 <meisuig98@gmail.com>
2020-06-09 05:16:48 +0000

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,6 @@
 class PostsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
-  
+
   def index
     @posts = Posts.find_by(book_id: params[:book_id])
   end
@@ -48,6 +47,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post = current_user.posts.find(params[:post_id])
     current_user.unpost(@post)
     @post.destroy
     flash[:success] = 'レビューを削除しました。'
@@ -60,10 +60,4 @@ class PostsController < ApplicationController
     params.require(:post).permit(:content)
   end
   
-  def correct_user
-    @post = current_user.posts.find_by(id: params[:id])
-    unless @post
-      redirect_to root_url
-    end
-  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :book
   
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :likes,  through: :favorites, source: :user
 end
